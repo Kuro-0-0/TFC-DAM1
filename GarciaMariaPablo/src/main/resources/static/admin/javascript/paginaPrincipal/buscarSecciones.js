0f352899c6a8944f57d485ed7aebf7e941d44a5a
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2025-05-05 01:01:30 +0200

    reOrganizar

--- GarciaMariaPablo/src/main/resources/static/admin/javascript/paginaPrincipal/buscarSecciones.js
+++ GarciaMariaPablo/src/main/resources/static/admin/javascript/paginaPrincipal/buscarSecciones.js
@@ -1,11 +1,19 @@
+/* Almacenar en una variable el input que se usara para filtar. */
 const inputBusqueda = document.getElementById('busqueda');
 
+/* Un listener para que cuando se mande el contenido del input se ejecute una funcion */
 inputBusqueda.addEventListener('input', function () {
+    /* Almacenar en una variable el valor buscado como minusculas */
     const filtro = this.value.toLowerCase();
-    const secciones = document.querySelectorAll('#adminAccordion .accordion-item');
+    /* Almacenar en una variables todos los elementos del HTML que tengan alguno de los ID o las clases buscadas */
+    const secciones = document.querySelectorAll('.accordion-item');
 
+    /* Iteramos sobre las secciones encontradas en la linea 9*/
     secciones.forEach(seccion => {
+        /* Almacenamos en la variable titulo, el texto como minuscula de los elementos cuya clase sea accordion-button dentro de las secciones
+        *  encontradas anteriormente. */
         const titulo = seccion.querySelector('.accordion-button').textContent.toLowerCase();
+        // Si titulo tiene lo que se escribio en el input el display se borra para que se muestre, si no, se oculta el elemento.
         if (titulo.includes(filtro)) {
             seccion.style.display = '';
         } else {
